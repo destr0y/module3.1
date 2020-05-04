08634e164a9c43061e1afc433037c305ecf1111a
Yury Bahdanau <yury_bahdanau1@epam.com>
2020-05-04 10:39:13 +0300

    add module 3.1

---
+++ Module_3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+
+namespace Module_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// Use this method to implement tasks
+        }
+
+    }
+    public class Task1
+    {
+        /// <summary>
+        /// Use this method to parse and validate user input
+        /// Throw ArgumentException if user input is invalid
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public int ParseAndValidateIntegerNumber(string source)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Multiplication(int num1, int num2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Task2
+    {
+        /// <summary>
+        /// Use this method to parse and validate user input
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public bool TryParseNaturalNumber(string input, out ulong result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable GetEvenNumbers(int naturalNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Task5
+    {
+        /// <summary>
+        /// Use this method to parse and validate user input
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public bool TryParseNaturalNumber(string input, out ulong result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RemoveDigitFromNumber(int source, int digitToRemove)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Module_3_tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Linq;
+using Module_3;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Test1
+    {
+        
+    }
+    public class Tests2
+    {
+        
+    }
+
+    public class Tests3
+    {
+       
+    }
+}
